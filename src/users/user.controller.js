4942ce4ceafb3f8338296e429486791bb45b90f1
Gideon Nti Boateng <87483815+ntiBoateng@users.noreply.github.com>
2023-03-03 12:02:19 +0000

    signup endpoint updated with activated boolean

--- src/users/user.controller.js
+++ src/users/user.controller.js
@@ -89,6 +89,7 @@ exports.Signup = async (req, res) => {
 
     return res.status(200).json({
       success: true,
+      activated: newUser.active,
       message: "Registration Success",
       referralCode: result.value.referralCode,
     });
